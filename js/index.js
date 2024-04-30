923188a09eb410f1f84ba7e34e6e83d021894239
Zawar Naeem <0.zawar.k.0@gmail.com>
2024-04-30 13:38:09 +0200

    Cambio de video de fondo para dispositivos móviles

--- js/index.js
+++ js/index.js
@@ -11,4 +11,21 @@ document.addEventListener("DOMContentLoaded", function () {
   menuIcon.addEventListener("click", function () {
     toggleMenu();
   });
+
+  const videoDesktop = document.getElementById("video-background-desktop");
+  const videoMobile = document.getElementById("video-background-mobile");
+
+  function toggleVideo() {
+    if (window.innerWidth <= 600) {
+      videoDesktop.style.display = "none"; // Ocultar el video de escritorio
+      videoMobile.style.display = "block"; // Mostrar el video móvil
+    } else {
+      videoDesktop.style.display = "block"; // Mostrar el video de escritorio
+      videoMobile.style.display = "none"; // Ocultar el video móvil
+    }
+  }
+
+  // Ejecutar toggleVideo al cargar y redimensionar la ventana
+  toggleVideo();
+  window.addEventListener("resize", toggleVideo);
 });
